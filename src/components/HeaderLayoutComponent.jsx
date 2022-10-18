1429db21df50b007303819b1adb5d8d5685a2826
Martynas <martynas@Martynass-MBP.lan>
2022-10-18 21:31:06 +0300

    updated header layout component with new routes

--- src/components/HeaderLayoutComponent.jsx
+++ src/components/HeaderLayoutComponent.jsx
@@ -5,10 +5,12 @@ function HeaderLayoutComponent() {
 
   return (
     <header className='header-container'>
-      <Link to='/login'>Login</Link>
+      <Link to='/'>Admin Panel</Link>
+      {/* <Link to='/login'>Login</Link>
       <Link to='/register'>Register</Link>
       <Link to='/add'> Add Customer</Link>
-      <Link to='/'>Customers List</Link>
+      <Link to='/customers'>Customers List</Link> */}
+      <Link to='/customers'>Customers</Link>
     </header>
   )
 }
