f527bdc40e1b88f516f5dd2ae19e4de2404c0673
Martynas <martynas@Martynass-MBP.lan>
2022-10-18 21:26:53 +0300

    added new routes with components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import LoginComponent from './components/LoginComponent';
+import RegisterComponent from './components/RegisterComponent';
+import AdminPanelComponent from './components/AdminPanelComponent';
+import CustomersPanelComponent from './components/CustomersPanelComponent';
+import AddCustomerComponent from './components/AddCustomerComponent';
 
 function App() {
   
   return (
    <Routes>
+    <Route path='/' element={<AdminPanelComponent />} />
     <Route path='/login' element={<LoginComponent />} />
+    <Route path='/register' element={<RegisterComponent />} />
+    <Route path='/customers' element={<CustomersPanelComponent />} />
+    <Route path='/add' element={<AddCustomerComponent />} />
+    <Route path='/list' />
    </Routes>
   );
 }
