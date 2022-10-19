fe76e796a2f62dafcd01ae6ace99e6dd7ca090b2
Martynas <martynas@Martynass-MBP.lan>
2022-10-19 20:43:57 +0300

    updated add customer component with authorized access

--- src/components/AddCustomerComponent.jsx
+++ src/components/AddCustomerComponent.jsx
@@ -2,17 +2,22 @@ import { useState } from "react";
 import CustomersPanelComponent from "./CustomersPanelComponent";
 
 function AddCustomerComponent() {
+  const token = localStorage.getItem('token');
+  const loggedInAdmin = localStorage.getItem('adminId');
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
   const [email, setEmail] = useState('');
   const [age, setAge] = useState('');
-  const [adminId, setAdminId] = useState('');
+  const [adminId, setAdminId] = useState(loggedInAdmin);
+
+
 
   const add = (event) => {
     event.preventDefault();
     fetch('http://localhost:8080/customers/add', {
       method: 'POST',
       headers: {
+        'Authorization': `Bearer ${token}`,
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
@@ -21,34 +26,21 @@ function AddCustomerComponent() {
         surname: surname,
         email: email,
         age: age,
-        adminId: adminId
+        admin_id: adminId
       }),
     })
 
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(error => console.log(error))
+    console.log(token);
+    console.log(loggedInAdmin);
     setName('');
     setSurname('');
     setEmail('');
     setAge('');
     setAdminId('');
   }
-
-  // const adminUsers = (bodyData) => {
-  //   fetch('http://localhost:8080/admin/users', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'        
-  //     },
-  //     body: JSON.stringify(bodyData)
-  //   })
-
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(error => console.log(error))
-  // }
 
   return (
     <div>
@@ -97,14 +89,14 @@ function AddCustomerComponent() {
             htmlFor="adminId">admin
           </label>
           <input 
-            type="text"
+            type="number"
             name="adminId"
             id="adminId"
             value={adminId}
             onChange={event => setAdminId(event.target.value)} />
           <button 
             type="submit" 
-            onClick={event => add(event)}>Login
+            onClick={event => add(event)}>Add
           </button>                                  
         </div>     
       </form>
