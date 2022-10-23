879c789a150dba28d6a324e8466bd235d4af4fe7
Martynas <martynas@Martynass-MBP.lan>
2022-10-23 22:24:50 +0300

    updated component with button

--- src/components/LogoutComponent.jsx
+++ src/components/LogoutComponent.jsx
@@ -1,6 +1,18 @@
 function LogoutComponent() {
   localStorage.removeItem("token");
   localStorage.removeItem("adminId");
+
+  return (
+
+    <div className='logout-button-container'>
+      <button
+        className="logout-button"
+        type="submit"
+        onClick={LogoutComponent}>Logout
+      </button>
+    </div>
+  )
+  
 };
 
 export default LogoutComponent;
