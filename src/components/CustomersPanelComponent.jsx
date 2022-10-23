0f59421ee4820178e25ee73193a4202960fa4b6b
Martynas <martynas@Martynass-MBP.lan>
2022-10-23 22:19:10 +0300

    added logout button from component

--- src/components/CustomersPanelComponent.jsx
+++ src/components/CustomersPanelComponent.jsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
 import '../style/HeaderStyle.css';
+import LogoutComponent from './LogoutComponent';
+
 
 function CustomersPanelComponent() {
 
   return (
     
     <div className='customers-panel-container header-style'>
-      <Link to='/'>Admin Panel</Link>
+      <Link to='/'>Home</Link>
       <Link to='/add'>Add Customer</Link>
-      <Link to='/list'>Customer List</Link>
+      <Link to='/list'>Customers List</Link>
+      <LogoutComponent />
     </div>
   )
 }
--- src/components/HeaderLayoutComponent.jsx
+++ src/components/HeaderLayoutComponent.jsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router-dom';
 import '../style/HeaderStyle.css';
+import LogoutComponent from './LogoutComponent';
 
 function HeaderLayoutComponent() {
 
   return (
+
     <header className='header-style'>
-      <Link to='/'>Admin Panel</Link>
-      {/* <Link to='/login'>Login</Link>
-      <Link to='/register'>Register</Link>
-      <Link to='/add'> Add Customer</Link>
-      <Link to='/customers'>Customers List</Link> */}
+      <Link to='/'>Home</Link>
       <Link to='/customers'>Customers</Link>
+      <LogoutComponent />
     </header>
   )
-}
+
+};
 
 export default HeaderLayoutComponent;
