e276ccea9e5056e0761d94effd681cc387c4b0a0
Martynas <martynas@Martynass-MBP.lan>
2022-10-23 22:23:42 +0300

    removed style class

--- src/components/AdminPanelComponent.jsx
+++ src/components/AdminPanelComponent.jsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
-import '../style/HeaderStyle.css';
+import '../style/AdminPanelStyle.css';
 
 function AdminPanelComponent() {
 
   return (
-    <div className='admin-panel-container header-style'>
+    
+    <div className='admin-panel-container'>
       <Link to='/login'>Login</Link>
       <Link to='/register'>Register</Link>
     </div>
   )
-}
+
+};
+
 
 export default AdminPanelComponent;
